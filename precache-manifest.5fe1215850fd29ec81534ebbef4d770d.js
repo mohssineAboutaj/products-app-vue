55b088e08cbe2148061c715f3e53d8edef2ad971
mohssineAboutaj <mohssineaboutaj1995@gmail.com>
2019-11-19 00:48:37 +0100

    Updates

---
+++ precache-manifest.5fe1215850fd29ec81534ebbef4d770d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66a9fcb20ba4d1e8bbdc",
+    "url": "/css/app.6b3d740d.css"
+  },
+  {
+    "revision": "5dce46bd48eb3e1bc0b763cbec7e5158",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66a9fcb20ba4d1e8bbdc",
+    "url": "/js/app.886c0397.js"
+  },
+  {
+    "revision": "2cb60bda80d970fe06d2",
+    "url": "/js/chunk-vendors.a62fc217.js"
+  },
+  {
+    "revision": "6032354c5b7a75feeca5902da39bc622",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
